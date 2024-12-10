c7cc87929ff4abb413163501247c2a506dae3348
DmitryAsl <aslamovdima10@yandex.ru>
2024-12-10 22:49:16 +0300

    add req and script_os

---
+++ script_os.py
@@ -0,0 +1,7 @@
+import os
+
+CURRENT_FILE = os.path.abspath(__file__)
+CURRENT_DIR = os.path.dirname(CURRENT_FILE)
+
+DATA_PATH = os.path.join(CURRENT_DIR, 'data')
+RESOURCES_PATH = os.path.join(CURRENT_DIR, 'resources')
